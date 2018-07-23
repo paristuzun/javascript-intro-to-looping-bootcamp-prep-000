e1b1a7c834ae57157f740f765c67f3e2c04497fe
Paris Tuzun <paristuzun@gmail.com>
2018-07-23 23:17:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,21 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-    array.push("I am 1 strange loop."); }
+    return "I am 1 strange loop."; }
     
     else { 
-      array.push("I am ${i} strange loops"); }
+      return "I am ${i} strange loops"; }
     }
     
     return array;
 }
+
+/*
+function testNum(a) {
+  if (a > 0) {
+    return "positive";
+  } else {
+    return "NOT positive";
+  }
+}*/
 
