e32cf82867186bb4a5251569a7bf6023a31ba044
Paris Tuzun <paristuzun@gmail.com>
2018-07-24 00:03:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,12 +18,12 @@ return "done"
 }
 
 function doWhileLoop(array) {
+  function maybeTrue() {
   return Math.random() >= 0.5
 }
  
 do {
   console.log('doo-bee-doo-bee-doo')
 } while (maybeTrue());
-  return array
 }
 
